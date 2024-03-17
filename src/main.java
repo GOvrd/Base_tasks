dfcc444f55c8d3d2152dc6173e6d234bdad62b3b
Дмитрий Макаренко <dimamak.t@gmail.com>
2024-03-17 20:24:57 +0300

    one commit

---
+++ src/main.java
@@ -0,0 +1,39 @@
+public class main {
+    public static void main(String[] args){
+        int task = 6;
+//        switch (task){
+//            case 1:
+//                System.out.println(Task1.task("01:00"));
+//                System.out.println(Task1.task("13:56"));
+//                System.out.println(Task1.task("10:60"));
+//                System.out.println(Task1.task("61:00"));
+//                break;
+//            case 2:
+//                System.out.println(Task2.task(4666));
+//                System.out.println(Task2.task(544));
+//                System.out.println(Task2.task(0));
+//                System.out.println(Task2.task(-1));
+//                System.out.println(Task2.task(-544));
+//                break;
+//            case 3:
+//                Task3.task();
+//                break;
+//            case 4:
+//                int[] mas1 = {1, 2, 3, 4};
+//                int[] mas2 = {2, 3};
+//                System.out.println(Task4.task(mas1, mas2));
+//                break;
+//            case 5:
+//                System.out.println(Task5.task("hTsii  s aimex dpus rtni.g"));
+//                break;
+//            case 6:
+//                //System.out.println(Task6.is_palindrom("12345321".toCharArray()));
+//                System.out.println(Task6.task(4021));
+//                break;
+//            default:
+//                System.out.println("Fail");
+//                break;
+//        }
+
+    }
+}
